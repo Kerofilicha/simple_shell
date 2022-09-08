e6d73444d170342a7d28d8e8eb286d1f9523231c
Kero Filicha <kerotulu@gmail.com>
2022-09-07 19:13:36 -0700

    Alias

--- f_alias_list.c
+++ f_alias_list.c
@@ -1,167 +1,115 @@
 #include "shell.h"
 
-static int not_init_alias_head = 1;
-static list_t *alias_head;
-
 /**
- * get_alias_head - Returns alias_head pointer
- *
- * Return: Pointer to alias head var
-*/
-list_t **get_alias_head()
+ * _myhistory - displays the history list, one command by line, preceded
+ *              with line numbers, starting at 0.
+ * @info: Structure containing potential arguments. Used to maintain
+ *        constant function prototype.
+ *  Return: Always 0
+ */
+int _myhistory(info_t *info)
 {
-	if (not_init_alias_head == 1)
-	{
-		alias_head = NULL;
-		not_init_alias_head = 0;
-	}
-	return (&alias_head);
-}
-
-/**
- * is_set_alias - Checks if the alias input is meant to set an alias
- * @alias_pair: String as alias arg input
- *
- * Return: 1 for true, 0 otherwise
-*/
-int is_set_alias(char *alias_pair)
-{
-	int i;
-
-	/* Check for the '=' sign */
-	/* the '=' sign shouldn't at indx 0 */
-	for (i = 1; alias_pair[i] != '\0'; i++)
-		if (alias_pair[i] == '=')
-			return (1);
-
+	print_list(info->history);
 	return (0);
 }
 
 /**
- * set_alias - adds or updates aliases
- * @alias_pair: String in the form name=value
-*/
-void set_alias(char *alias_pair)
-{
-	char tmp_buff[250];
-	int first_eq, last_eq;
-	list_t *curr;
-	list_t **alias_addrs = get_alias_head();
-
-	/* Find first '=' sign */
-	for (first_eq = 1; alias_pair[first_eq] != '\0'; first_eq++)
-		if (alias_pair[first_eq] == '=')
-			break;
-
-	/* Copy the text before '=' */
-	strncpy(tmp_buff, alias_pair, first_eq);
-	tmp_buff[first_eq] = '\0';
-	/* Append = and ' symbols */
-	_strcat(tmp_buff, "='");
-
-	/* Find last eq in case of putting '=' secuentially */
-	for (last_eq = first_eq; alias_pair[last_eq] != '\0'; last_eq++)
-		if (alias_pair[last_eq] != '=')
-			break;
-
-	/* Copy the second part of the alias */
-	_strcat(tmp_buff, &alias_pair[last_eq]);
-	_strcat(tmp_buff, "'");
-
-	/* Add alias to the global list */
-	for (curr = *alias_addrs; curr != NULL; curr = curr->next)
-		if (_strncmp(curr->str, alias_pair, first_eq) == 0 &&
-				(curr->str)[first_eq] == '=')
-		{ /* Update already existing alias */
-			free(curr->str);
-			curr->str = _strdup(tmp_buff);
-			return;
-		}
-
-	/* Add new alias to the list */
-	add_node_end(alias_addrs, tmp_buff);
-}
-
-/**
- * handle_alias_args - Evaluates alias input command and decides what to do
- * @commands: Arrays of commands
- * @out_addrs: Pointer to out's head node
+ * unset_alias - sets alias to string
+ * @info: parameter struct
+ * @str: the string alias
  *
- * Return: -1 if error happens, 0 otherwise
-*/
-int handle_alias_args(char **commands, list_t **out_addrs)
+ * Return: Always 0 on success, 1 on error
+ */
+int unset_alias(info_t *info, char *str)
 {
-	int i, len, was_alias;
-	int status = 0;
-	list_t *curr;
-	list_t **alias_addrs = get_alias_head();
+	char *p, c;
+	int ret;
 
-	set_process_exit_code(0);
-	for (i = 1; commands[i] != NULL; i++)
-	{
-		was_alias = 0;
-		len = _strlen(commands[i]);
-		/* Check that if user is trying to print an alias */
-		for (curr = *alias_addrs; curr != NULL; curr = curr->next)
-		{
-			if (_strncmp(curr->str, commands[i], len) == 0 && (curr->str)[len] == '=')
-			{ /* Means to list an alias */
-				was_alias = 1;
-				add_node_end(out_addrs, curr->str);
-				break;
-			}
-		}
-		if (was_alias)
-			continue;
-		/* Here, user is trying to set an alias or print an unexistent one */
-		if (is_set_alias(commands[i]))
-			set_alias(commands[i]); /* Validate that the set format is fine */
-		else /* Print errors */
-		{
-			status = -1;
-			set_process_exit_code(1);
-			_puts("alias: ");
-			_puts(commands[i]);
-			_puts(" not found\n");
-		}
-	}
-
-	return (status);
+	p = _strchr(str, '=');
+	if (!p)
+		return (1);
+	c = *p;
+	*p = 0;
+	ret = delete_node_at_index(&(info->alias),
+		get_node_index(info->alias, node_starts_with(info->alias, str, -1)));
+	*p = c;
+	return (ret);
 }
 
 /**
- * handle_aliases - Handle alias replacement
- * @commands: Arrays of commands
-*/
-void handle_aliases(char **commands)
+ * set_alias - sets alias to string
+ * @info: parameter struct
+ * @str: the string alias
+ *
+ * Return: Always 0 on success, 1 on error
+ */
+int set_alias(info_t *info, char *str)
 {
-	list_t *curr;
-	list_t **alias_addrs = get_alias_head();
-	int cmd_len = _strlen(commands[0]);
-	char *str;
-	char tmp_buff[250];
-	int i, alias_len = 0;
+	char *p;
 
-	if (commands[0] == NULL)
-		return;
+	p = _strchr(str, '=');
+	if (!p)
+		return (1);
+	if (!*++p)
+		return (unset_alias(info, str));
 
-	/* Initialize buffer */
-	for (i = 0; i < 250; i++)
-		tmp_buff[i] = '\0';
+	unset_alias(info, str);
+	return (add_node_end(&(info->alias), str, 0) == NULL);
+}
 
-	/* Search if a command is an alias */
-	for (curr = *alias_addrs; curr != NULL; curr = curr->next)
+/**
+ * print_alias - prints an alias string
+ * @node: the alias node
+ *
+ * Return: Always 0 on success, 1 on error
+ */
+int print_alias(list_t *node)
+{
+	char *p = NULL, *a = NULL;
+
+	if (node)
 	{
-		str = curr->str;
-		if (_strncmp(commands[0], str, cmd_len) == 0 && str[cmd_len] == '=')
-		{ /* the command is an alias */
-			alias_len = _strlen(&str[cmd_len + 2]); /* +2 beacause of "='" chars */
-			strncpy(tmp_buff, &str[cmd_len + 2], alias_len - 1);
-			tmp_buff[alias_len] = '\0';
-			/* Free and then update the command */
-			free(commands[0]);
-			commands[0] = duplicate_string(tmp_buff);
-			break;
-		}
+		p = _strchr(node->str, '=');
+		for (a = node->str; a <= p; a++)
+			_putchar(*a);
+		_putchar('\'');
+		_puts(p + 1);
+		_puts("'\n");
+		return (0);
 	}
+	return (1);
+}
+
+/**
+ * _myalias - mimics the alias builtin (man alias)
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ *  Return: Always 0
+ */
+int _myalias(info_t *info)
+{
+	int i = 0;
+	char *p = NULL;
+	list_t *node = NULL;
+
+	if (info->argc == 1)
+	{
+		node = info->alias;
+		while (node)
+		{
+			print_alias(node);
+			node = node->next;
+		}
+		return (0);
+	}
+	for (i = 1; info->argv[i]; i++)
+	{
+		p = _strchr(info->argv[i], '=');
+		if (p)
+			set_alias(info, info->argv[i]);
+		else
+			print_alias(node_starts_with(info->alias, info->argv[i], '='));
+	}
+
+	return (0);
 }
